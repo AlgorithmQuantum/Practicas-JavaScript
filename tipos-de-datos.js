d56082119c7cf3cd1908ac21c192b561f4bd2b33
AlgorithQuantum <angelcalleja55@hotmail.com>
2026-04-19 14:18:37 -0600

    primera practica del modulo de JavaScript

---
+++ tipos-de-datos.js
@@ -0,0 +1,22 @@
+/* Practica 1 de DEVF tipos de datos en JavaScript */
+
+console.log(typeof 42)
+console.log(typeof 'Veinticinco')
+console.log(typeof -666)
+console.log(typeof true)
+console.log(typeof 0)
+console.log(typeof "")
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof FALSE)
+
+/* tipo de dato objeto*/
+console.log(typeof new Date())
+/* tipo de dato funcion */
+console.log(typeof function(){})
+/* tipo de dato simbolo*/
+console.log(typeof Symbol("id"))
+/* tipo de dato bigint*/
+console.log(typeof 123n)
+/* tipo de dato simbolo*/
+console.log(typeof new Set())
